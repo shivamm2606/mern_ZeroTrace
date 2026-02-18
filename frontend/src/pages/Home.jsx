70f6f782fa3362a5550137c6e00cc0c7a5eb1d3f
Shivam <shivamm.2606@gmail.com>
2026-02-18 23:40:27 +0530

    add compose form in home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+
 import ComposeForm from '../components/ComposeForm';
 
 const Home = () => {
     return (
         <div className="min-h-screen bg-zinc-950 flex flex-col items-center pt-10 p-4">
             <header className="mb-12 text-center animate-fade-in relative z-10">
-                <div className="inline-flex items-center justify-center mb-2">
+                <div className="inline-flex  justify-center items-center mb-2">
                     <h1 className="text-4xl px-4 text-slate-100">
                         <span className="font-bold">Zero</span>
                         <span className="font-medium text-slate-250">Trace</span>
