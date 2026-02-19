5406310fdef5e3db0580730298d9e1b8424b60b3
Shivam <shivamm.2606@gmail.com>
2026-02-20 01:19:53 +0530

    fix server

--- backend/src/server.js
+++ backend/src/server.js
@@ -44,7 +44,7 @@ app.use(cors({
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
     max: 100, // limit each IP to 100 requests per windowMs
-    message: 'Too many requests from this IP, please try again later',
+    message: 'Too many requests from this IP please try again later',
 });
 
 app.use('/api', limiter);
