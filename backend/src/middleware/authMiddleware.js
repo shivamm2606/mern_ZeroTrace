624fccb0638992bbe5a381237281c09e9ae5bd97
Shivam <shivamm.2606@gmail.com>
2026-03-09 00:51:28 +0530

    fix export issue

--- backend/src/middleware/authMiddleware.js
+++ backend/src/middleware/authMiddleware.js
@@ -1,25 +1,27 @@
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 
-export const protect = (req, res, next) => {
-    let token;
+const protect = (req, res, next) => {
+  let token;
 
-    if (req.cookies && req.cookies.adminToken) {
-        token = req.cookies.adminToken;
-    }
+  if (req.cookies && req.cookies.adminToken) {
+    token = req.cookies.adminToken;
+  }
 
-    if (!token) {
-        res.status(401);
-        const error = new Error('Not authorized, no token!');
-        return next(error);
-    }
+  if (!token) {
+    res.status(401);
+    const error = new Error("Not authorized, no token!");
+    return next(error);
+  }
 
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.admin = decoded;
-        next();
-    } catch (error) {
-        res.status(401);
-        const err = new Error('Not authorized, token failed');
-        next(err);
-    }
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.admin = decoded;
+    next();
+  } catch (error) {
+    res.status(401);
+    const err = new Error("Not authorized, token failed");
+    next(err);
+  }
 };
+
+export { protect };
